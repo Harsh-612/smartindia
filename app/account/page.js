5ca421b65eeafbfd6f435c691e01c27352dd8e20
Harsh-612 <hchaturvedi126@gmail.com>
2023-10-05 19:33:14 +0530

    changed the login page a bit

--- app/account/page.js
+++ app/account/page.js
@@ -18,11 +18,10 @@ const page = () => {
   }, [user]);
 
   return (
-    <div className="w-full accountwrapper relative bg-sky-50">
-      <Nav />
-      <div className="h-full w-full flex justify-center items-center">
-        <div className=" h-96 border bg-white w-1/3 mt-20 rounded-lg border-gray-400 flex items-center flex-col shadow-2xl">
-          <h1 className="text-3xl font-bold mb-10 mt-8">Login to LawWiz</h1>
+    <div className="w-full accountwrapper bg-sky-50 flex">
+      <div className="h-full w-full flex justify-start items-center">
+        <div className=" h-full bg-white w-1/2 rounded-lg  flex items-center flex-col justify-center gap-8">
+          <h1 className="text-3xl font-bold ">Login to LawWiz</h1>
           <form className="w-full flex flex-col gap-4 items-center">
             <input
               type="text"
@@ -35,18 +34,19 @@ const page = () => {
               className="border border-gray-400 w-3/4 text-lg px-2 py-1"
             />
           </form>
-          <div className="flex flex-col items-center mt-8 w-3/4">
+          <div className="flex flex-col items-center  w-3/4">
             <button class="light text-white py-2 text-sm rounded w-full">
               Log-in/Register
             </button>
-            <div className="text-gray-600 text-sm mt-0.5">Or</div>
+            <div className="text-gray-600 text-sm mt-1">Or</div>
+            <button
+              class="light text-white w-full  gap-2 text-sm py-2 rounded mt-1"
+              onClick={login}
+            >
+              Sign-In with Google
+            </button>
           </div>
-          <button
-            class="light text-white w-3/4 text-sm py-2 rounded mt-1"
-            onClick={login}
-          >
-            Sign-In with Google
-          </button>
+
           <div
             onClick={() => {
               auth.signOut();
@@ -54,6 +54,9 @@ const page = () => {
           >
             {user ? user.displayName + "  " + user.email : ""}
           </div>
+        </div>
+        <div className="w-1/2 h-full flex justify-center items-center">
+          <img src="/Images/justicevector.png" />
         </div>
       </div>
     </div>
--- app/firebase.js
+++ app/firebase.js
@@ -1,12 +1,7 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyAmp5b-s-8il2GSWPQt5tSASoL3j-U4yRI",
   authDomain: "next-auth-7df41.firebaseapp.com",
@@ -17,7 +12,6 @@ const firebaseConfig = {
   measurementId: "G-ZHGDWBTV0N",
 };
 
-// Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth();
 export const database = getFirestore(app);
